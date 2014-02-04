bdbc9a29069d4c9f1528cc3bf4cbb8b5402e6e8a
Rainbough <itintern@ITs-iMac-2.local>
2014-02-04 16:22:53 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LandesBox::Application.config.secret_key_base = 'e195e9918350c27e8e3c6b2d05e906c25b871502bd907916afd5575dd660f8ca69fa6b8c299ed2e56b4370c078857814a211c9a803e75bf500a1734ff22420ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LandesBox::Application.config.session_store :cookie_store, key: '_landes_box_session'
